ee55a0160d4a2c9759e58f02dad7cb4048c1fa3d
Sean Anderson <84966214+seanandersxn@users.noreply.github.com>
2025-04-03 16:29:53 +0200

    Built Mode. Added Command to Add Data from Excel Sheet to SQLite Database

---
+++ core/book/management/commands/add_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from django.core.management.base import BaseCommand
+from book.models import Book
+from sqlalchemy import create_engine
+
+class Command(BaseCommand):
+    help = "A command to add data from an Excel file to the database"
+
+    def handle(self, *args, **options):
+        excel_file = 'books.xlsx'
+        df = pd.read_excel(excel_file)
+        
+        engine = create_engine('sqlite:///db.sqlite3')
+        df.to_sql(Book._meta.db_table, if_exists='replace', con=engine, index=True)
